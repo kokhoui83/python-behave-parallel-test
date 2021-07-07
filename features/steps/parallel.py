3f547be1f0a4edc4d4e13458fef6ff50e8fb746d
brian lim <kokhoui.lim@duerrdental.com>
2021-07-07 19:01:08 +0800

    don't wait for result

--- features/steps/parallel.py
+++ features/steps/parallel.py
@@ -2,7 +2,7 @@ from concurrent.futures import ThreadPoolExecutor, wait
 from behave import step
 from behave.__main__ import main as behave_main
 
-executor = ThreadPoolExecutor(max_workers=2)
+executor = ThreadPoolExecutor(max_workers=5)
 
 def feature_executor(*args):
     context = args[0]
@@ -13,4 +13,4 @@ def feature_executor(*args):
 @step('Executing {feature} {scenario}')
 def step_impl(context, feature, scenario):
     test = executor.submit(feature_executor, context, feature, scenario)
-    test.result()
+    # test.result()
