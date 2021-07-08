6ef95807a083bfb0ba13a0371c978ae6210acd06
brian lim <kokhoui.lim@duerrdental.com>
2021-07-08 11:34:20 +0800

    Add error handling with try/except

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,5 @@
 from behave import fixture, use_fixture
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed, CancelledError, TimeoutError
 
 @fixture
 def create_threadpool(context):
@@ -12,11 +12,22 @@ def create_threadpool(context):
     yield context.threadpool['executor']
 
     # check result if needed
-    # for future in as_completed(context.threadpool['futures']):
-    #     print(future.result())
+    for future in as_completed(context.threadpool['futures']):
+        try:
+            result = future.result()
+            print(result)
+        except CancelledError as ce:
+            print(ce)
+        except TimeoutError as te:
+            print(te)
+        except Exception as e:
+            print(e)
     
     # cleanup threadpool once all work is done
-    context.threadpool['executor'].shutdown(wait=True)
+    try:
+        context.threadpool['executor'].shutdown(wait=True)
+    except RuntimeError as re:
+        print(re)
 
 
 def before_tag(context, tag):
--- features/steps/parallel.py
+++ features/steps/parallel.py
@@ -5,10 +5,13 @@ def feature_executor(*args):
     context = args[0]
     feature = args[1]
     scenario = args[2]
-    behave_main(f'-i "{feature}" -n "{scenario}" --no-capture')
+    return behave_main(f'-i "{feature}" -n "{scenario}" --no-capture')
 
 @step('Executing {feature} {scenario}')
 def step_impl(context, feature, scenario):
-    executor = context.threadpool['executor']
-    test = executor.submit(feature_executor, context, feature, scenario)
-    context.threadpool['futures'].append(test)
+    try:
+        executor = context.threadpool['executor']
+        test = executor.submit(feature_executor, context, feature, scenario)
+        context.threadpool['futures'].append(test)
+    except Exception as e:
+        print(e)
